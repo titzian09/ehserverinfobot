abb4f49ea7029ad06484a161c723e1caeec0df90
Titzian <98521818+titzian09@users.noreply.github.com>
2025-04-09 13:20:19 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,72 @@
+import nextcord
+from nextcord.ext import commands, tasks
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+
+token = os.getenv("DISCORD_TOKEN")
+owner_id = int(os.getenv("OWNER_ID"))
+channel_id = int(os.getenv("CHANNEL_ID"))
+guild_id = int(os.getenv("GUILD_ID"))
+join_code = os.getenv("JOIN_CODE")
+join_link = "https://www.roblox.com/games/start?placeId=7711635737&launchData=joinCode%3D" + join_code
+bot = commands.Bot(command_prefix="!", intents=nextcord.Intents.all())
+guild = None
+channel = None
+
+@bot.event
+async def on_ready():
+    global guild
+    global channel
+    guild = bot.get_guild(guild_id)
+    channel = bot.get_channel(channel_id)
+    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
+    print(f"Connected to guild: {guild.name} (ID: {guild.id})")
+    print(f"Channel: {channel.name} (ID: {channel.id})")
+    update_status.start()
+
+def get_server_info():
+    servers_url = f"https://api.emergency-hamburg.com/public/servers"
+    response = requests.get(servers_url).json()
+
+    # Step 2: Search for the server with the matching join code
+    target_server = None
+    for server in response:
+        if server.get("ownerId") == owner_id:  # Join codes are case-insensitive in some cases
+            target_server = server
+            break
+    return target_server
+
+def create_embed():
+    server_info = get_server_info()
+    players = 0
+    max_players = 42
+    status = "🔴"
+    if server_info:
+        players = server_info['currentPlayers']
+        max_players = server_info['maxPlayers']
+        status = "🟢"
+
+    embed = nextcord.Embed(
+        title="Server Status",
+        description="Current server status and player count.",
+        color=nextcord.Color.blue(),
+    )
+    embed.add_field(name="Status", value=status, inline=False)
+    embed.add_field(name="Players", value=f"{players}/{max_players}", inline=False)
+    embed.add_field(name="Join Code", value=join_code, inline=False)
+    embed.add_field(name="Join Link", value=f"[Click here to join]({join_link})", inline=False)
+    embed.set_footer(text="Updated every minute.")
+    return embed
+
+@tasks.loop(minutes=1)
+async def update_status():
+    global channel
+    if channel:
+        embed = create_embed()
+        await channel.purge(limit=1)
+        await channel.send(embed=embed)
+
+bot.run(token)
---
+++ reade.md
@@ -0,0 +1,4 @@
+# DISCORD BOT TO DISPLAY INFO FROM EMERGENCY HAMBURG SERVERS (A ROBLOX GAME)
+- It can only get your data if your server is in the top 99 in the server list on the emergency hamburg website
+- you have to put your discord bot token, roblox user id, guild id, channel id and join code in the .env file for the bot to work
+- i made this bot in 20 minutes the code is not really good
